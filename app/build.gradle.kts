456dad9c48ac8062b0a903c6f02d20d3cce74b2b
cladzsan <claudio_santiago@dlsu.edu.ph>
2024-06-19 22:39:09 +0800

    To-Do list page header updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,7 @@ dependencies {
 
     // added dependencies
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.4.0")
+
 
 }
