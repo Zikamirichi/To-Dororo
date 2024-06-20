bc945d3bbf00ef695359811c5a3cc6ae0bc7b2bb
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-06-20 15:26:19 +0800

    backend for help page done

--- app/src/main/java/com/mobdeve/s11and12/group1/to_dororo/VirtualPetFragment.kt
+++ app/src/main/java/com/mobdeve/s11and12/group1/to_dororo/VirtualPetFragment.kt
@@ -1,10 +1,13 @@
 package com.mobdeve.s11and12.group1.to_dororo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import com.mobdeve.s11and12.group1.to_dororo.HelpActivity
 
 class VirtualPetFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,17 @@ class VirtualPetFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_virtual_pet, container, false)
+        val view = inflater.inflate(R.layout.fragment_virtual_pet, container, false)
+
+        // Find the help_icon ImageButton
+        val helpIcon = view.findViewById<ImageButton>(R.id.help_icon)
+
+        // Set OnClickListener for help_icon
+        helpIcon.setOnClickListener {
+            // Start HelpActivity
+            startActivity(Intent(requireContext(), HelpActivity::class.java))
+        }
+
+        return view
     }
 }
